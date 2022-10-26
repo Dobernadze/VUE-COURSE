68b99c97ae6d6e2bb89c4fc3eeb0e0544dcf70c7
Dobernadze <tima7723@gmail.com>
2022-10-26 17:14:40 +0600

    37:4157

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+
 <template>
  <div class="container mt-3">
   <div class="grid">
@@ -8,16 +9,22 @@
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda repellat ipsam neque explicabo, ipsum ipsa vitae at et commodi recusandae non ducimus deleniti eum laboriosam vel, id reiciendis sunt maxime.
         </p>
       </div>
+    
     </div>
+    <Customer name="Dan" age="25" designation = "Engineer"/>
   </div>
-  <Customer/>
+
+
  </div>
-  
 </template>
 
 <script>
-
+import Customer from "./components/Customer";
+export default{
+  components:{Customer}
+}
 </script>
+
 
 <style>
   @import "./styles.css";
--- src/components/Customer.vue
+++ src/components/Customer.vue
@@ -1,20 +1,34 @@
 <template>
-    <div class = "container">
-        <div class = "row">
-            <div class = "col">
-                <p class = "h3">Customer</p>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class ="h3">Customer</p>
+                <ul class ="list-group">
+                    <li class="list-group-item">
+                        Name:{{name}}
+                    </li>
+                    <li class="list-group-item">
+                        Age:{{age}}
+                    </li>
+                    <li class="list-group-item">
+                        Designation:{{designation}}
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
-    <h2> Customer</h2>
 </template>
 
 <script>
 export default{
-    name: "Customer"
-}
+    name: "Customer",
+    props:{
+        name:{type: String, required: true},
+        age:{type: Number, required: true},
+        designation:{type: String, required: true},
+    }
+};
 </script>
 
 <style scoped>
-
 </style>
